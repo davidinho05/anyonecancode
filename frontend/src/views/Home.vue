a53e4a2169b854a589ff2840fdd90356de6f463e
davidinho05 <107325223+davidinho05@users.noreply.github.com>
2023-06-13 14:34:12 -0500

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,7 +7,7 @@
         <div id="top"></div>
         <div id="bottom"></div>
         <b-navbar-item id="fontnavbar" class="title is-2" style="color: SteelBlue">
-          Davids Miligram
+          Davidsgram
         </b-navbar-item>
       </template>
     </b-navbar>
